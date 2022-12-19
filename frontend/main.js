44cc55cb348b68e98966751eabfa875f0c4327d7
dave <le.tt@outlook.com>
2022-12-19 20:20:40 +0100

    Updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+//when the domstruture is loaded, run getvisitcout
+window.addEventListener('DOMContentLoaded', (event)=>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("website called function api.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
